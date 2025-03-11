e531ae032aaec9af37cc3487a12d90268a9acad7
LeviathanDan <daniel.gladkiy@gmail.com>
2025-03-11 10:20:17 +0100

    Solved 3 task

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
@@ -1,11 +1,55 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+#text = input("Type your message:\n").lower()
+#shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs
+
+
+
+def encrypt(original_text, shift_amount):
+    encrypted_text = ''
+
+    for letter in original_text:
+        new_position = alphabet.index(letter) + shift_amount
+        new_position %= len(alphabet)
+        encrypted_text += alphabet[new_position]
+
+    print(encrypted_text)
+
+encrypt("hello", 1)
+
+
+# encrypted_word =[]
+# def encrypt(original_text, shift_amount):
+#     listed_word =  list(original_text)
+#     indexes_of_word = []
+#     for letter in listed_word:
+#         indexes_of_word.append(int(alphabet.index(letter)) + shift_amount)
+#
+#     for index in indexes_of_word:
+#         encrypted_word.append(alphabet[index])
+#
+#     print(indexes_of_word)
+#     print(''.join(encrypted_word))
+
+
+
+
+# final_word = []
+# indexes = [7,4,11,11,14]
+# new_indexes = []
+# for kek in indexes:
+#     kek += 1
+#     new_indexes.append(kek)
+#     final_word.append(alphabet[kek])
+
+
+
+
+# encrypt('hello', 1)
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
